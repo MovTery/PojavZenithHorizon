12ce4a5780a2d532a81088ab99ac738f191615e2
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-03-15 13:32:56 +0800

    重构项目

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/ControlString.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/ControlString.java
@@ -8,4 +8,7 @@ public class ControlString extends AppCompatActivity {
     public String BUTTON = getString(R.string.zh_add_control_button);
     public String DRAWER = getString(R.string.zh_add_control_drawer);
     public String STRING_LOAD_FAILED = getString(R.string.zh_load_failed);
+
+    public ControlString() {
+    }
 }
