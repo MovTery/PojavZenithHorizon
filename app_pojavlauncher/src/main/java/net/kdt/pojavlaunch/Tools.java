b25ea9ffcebd01760ac793517d71afe69addf790
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-03-19 18:50:19 +0800

    优化布局

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -508,11 +508,16 @@ public final class Tools {
             if(fullscreen){
                 if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
                     decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LOW_PROFILE
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+                    );
+
+                    activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 }
             }else{
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
