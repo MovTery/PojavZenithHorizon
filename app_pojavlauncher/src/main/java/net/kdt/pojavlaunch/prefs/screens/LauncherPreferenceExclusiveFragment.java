dcab4f7ddc0101f7b1dd84c89750c019337df90e
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-03-22 14:39:35 +0800

    动画：设置速率

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExclusiveFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExclusiveFragment.java
@@ -1,9 +1,14 @@
 package net.kdt.pojavlaunch.prefs.screens;
 
 import static net.kdt.pojavlaunch.Tools.animationRate;
-import static net.kdt.pojavlaunch.prefs.LauncherPreferences.PREF_ANIMATION_RATE;
+import static net.kdt.pojavlaunch.prefs.LauncherPreferences.DEFAULT_PREF;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.preference.Preference;
 
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.prefs.CustomSeekBarPreference;
@@ -14,17 +19,34 @@ public class LauncherPreferenceExclusiveFragment extends LauncherPreferenceFragm
         addPreferencesFromResource(R.xml.pref_exclusive);
 
         // 获取值
-        long rate = PREF_ANIMATION_RATE;
+        int rate = DEFAULT_PREF.getInt("animationRate", 300);
 
         CustomSeekBarPreference seek = requirePreference("animationRate", CustomSeekBarPreference.class);
         seek.setMin(0);
         seek.setMax(1000);
-        seek.setValue((int) rate);
+        seek.setValue(rate);
         seek.setSuffix(" ms");
 
-        seek.setOnPreferenceChangeListener((preference, newValue) -> {
-            animationRate(requireContext(), (Long) newValue);
-            return true;
+        seek.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
+                if (newValue instanceof Integer) {
+                    try {
+                        long rate = (long) newValue;
+                        animationRate(requireContext(), rate);
+                        return true;
+                    } catch (NumberFormatException e) {
+                        Log.e("LauncherPreferenceExclusiveFragment", "Invalid number format for animation rate: " + newValue);
+                        animationRate(requireContext(), 300L);
+                        return false;
+                    }
+                } else {
+                    Log.e("LauncherPreferenceExclusiveFragment", "Unexpected type for newValue: " + newValue.getClass().getName());
+                    animationRate(requireContext(), 300L);
+                    return false;
+                }
+            }
         });
     }
 }
