651fc45a6cce19d00e8f942a0561a88869dc1ce7
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-03-22 15:49:46 +0800

    动画：设置速率

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExclusiveFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExclusiveFragment.java
@@ -6,16 +6,18 @@ import android.os.Bundle;
 
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.prefs.CustomSeekBarPreference;
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 
 public class LauncherPreferenceExclusiveFragment extends LauncherPreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle b, String str) {
         addPreferencesFromResource(R.xml.pref_exclusive);
 
+        int rate = LauncherPreferences.PREF_ANIMATION_RATE;
+
         CustomSeekBarPreference seek = requirePreference("animationRate", CustomSeekBarPreference.class);
-        seek.setMin(0);
-        seek.setMax(1500);
-        seek.setValue(DEFAULT_PREF.getInt("animationRate", 300));
+        seek.setRange(0, 1500);
+        seek.setValue(rate);
         seek.setSuffix(" ms");
     }
 }
