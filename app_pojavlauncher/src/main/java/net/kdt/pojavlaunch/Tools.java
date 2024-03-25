85b5f40972c838c066287c01d329bbcb5bc52464
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-03-25 15:15:31 +0800

    本地整合包安装

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -114,6 +114,7 @@ public final class Tools {
     public static String NATIVE_LIB_DIR;
     public static String DIR_DATA; //Initialized later to get context
     public static File DIR_CACHE;
+    public static File DIR_GAME_MODPACK;
     public static String MULTIRT_HOME;
     public static String LOCAL_RENDERER = null;
     public static int DEVICE_ARCHITECTURE;
