44bc2e19cf5c6a20d1cd3362434134be172c7b17
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-04-01 00:34:37 +0800

    调整设置屏幕

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/SettingsActivity.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/SettingsActivity.java
@@ -3,6 +3,7 @@ package net.kdt.pojavlaunch;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,15 +16,7 @@ import net.kdt.pojavlaunch.prefs.screens.LauncherPreferenceVideoFragment;
 
 public class SettingsActivity extends BaseActivity {
     private ImageButton mReturnButton, mVideoButton, mControlsButton, mJavaButton, mMiscButton, mPojavZHButton, mExperimentalButton;
-
-    private void mSettingsOptionsLocked(View view) {
-        mVideoButton.setClickable(view != mVideoButton);
-        mControlsButton.setClickable(view != mControlsButton);
-        mJavaButton.setClickable(view != mJavaButton);
-        mMiscButton.setClickable(view != mMiscButton);
-        mPojavZHButton.setClickable(view != mPojavZHButton);
-        mExperimentalButton.setClickable(view != mExperimentalButton);
-    }
+    private View mIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +27,66 @@ public class SettingsActivity extends BaseActivity {
         mReturnButton.setOnClickListener(v -> finish());
 
         mVideoButton.setOnClickListener(v -> {
-            mSettingsOptionsLocked(v);
+            onButtonClick(v);
             swapFragment(LauncherPreferenceVideoFragment.class, LauncherPreferenceVideoFragment.TAG);
         });
         mControlsButton.setOnClickListener(v -> {
-            mSettingsOptionsLocked(v);
+            onButtonClick(v);
             swapFragment(LauncherPreferenceControlFragment.class, LauncherPreferenceControlFragment.TAG);
         });
         mJavaButton.setOnClickListener(v -> {
-            mSettingsOptionsLocked(v);
+            onButtonClick(v);
             swapFragment(LauncherPreferenceJavaFragment.class, LauncherPreferenceJavaFragment.TAG);
         });
         mMiscButton.setOnClickListener(v -> {
-            mSettingsOptionsLocked(v);
+            onButtonClick(v);
             swapFragment(LauncherPreferenceMiscellaneousFragment.class, LauncherPreferenceMiscellaneousFragment.TAG);
         });
         mPojavZHButton.setOnClickListener(v -> {
-            mSettingsOptionsLocked(v);
+            onButtonClick(v);
             swapFragment(LauncherPreferenceExclusiveFragment.class, LauncherPreferenceExclusiveFragment.TAG);
         });
         mExperimentalButton.setOnClickListener(v -> {
-            mSettingsOptionsLocked(v);
+            onButtonClick(v);
             swapFragment(LauncherPreferenceExperimentalFragment.class, LauncherPreferenceExperimentalFragment.TAG);
         });
+
+        initialize();
+    }
+
+    private void initialize() {
+        mVideoButton.setClickable(false);
+        setIndicatorPosition(mVideoButton);
+    }
+
+    private void onButtonClick(View view) {
+        mVideoButton.setClickable(view == mVideoButton);
+        mControlsButton.setClickable(view == mControlsButton);
+        mJavaButton.setClickable(view == mJavaButton);
+        mMiscButton.setClickable(view == mMiscButton);
+        mPojavZHButton.setClickable(view == mPojavZHButton);
+        mExperimentalButton.setClickable(view == mExperimentalButton);
+
+        //根据被点击的按钮设置小条的位置
+        setIndicatorPosition(view);
+    }
+
+    private void setIndicatorPosition(View button) {
+        if (button == null) {
+            return;
+        }
+
+        int[] buttonLocation = new int[2];
+        button.getLocationOnScreen(buttonLocation);
+        int buttonTop = buttonLocation[1] - ((RelativeLayout.LayoutParams) button.getLayoutParams()).topMargin;
+        int buttonLeft = buttonLocation[0] - ((RelativeLayout.LayoutParams) button.getLayoutParams()).leftMargin;
+
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mIndicator.getLayoutParams();
+        params.topMargin = buttonTop;
+        params.leftMargin = buttonLeft + (button.getWidth() - mIndicator.getWidth()) / 2;
+        mIndicator.setLayoutParams(params);
+
+        mIndicator.setVisibility(View.VISIBLE);
     }
 
     private void bindViews(){
@@ -67,6 +97,8 @@ public class SettingsActivity extends BaseActivity {
         mMiscButton = findViewById(R.id.misc_settings);
         mPojavZHButton = findViewById(R.id.pojav_zh_settings);
         mExperimentalButton = findViewById(R.id.experimental_settings);
+
+        mIndicator = findViewById(R.id.settings_indicator);
     }
 
     private void swapFragment(Class<? extends Fragment> fragmentClass, String fragmentTag) {
