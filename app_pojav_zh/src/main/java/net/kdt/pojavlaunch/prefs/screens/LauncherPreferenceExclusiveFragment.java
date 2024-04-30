f8d44274b54abeee305099c2823823d7e878ab95
movte <212824502@qq.com>
2024-04-30 23:40:32 +0800

    更新[浅色模式]：手动设置主题

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExclusiveFragment.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExclusiveFragment.java
@@ -1,6 +1,9 @@
 package net.kdt.pojavlaunch.prefs.screens;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+
+import androidx.preference.Preference;
 
 import net.kdt.pojavlaunch.R;
 
@@ -9,5 +12,19 @@ public class LauncherPreferenceExclusiveFragment extends LauncherPreferenceFragm
     @Override
     public void onCreatePreferences(Bundle b, String str) {
         addPreferencesFromResource(R.xml.pref_exclusive);
+        Preference launcherTheme = findPreference("launcherTheme");
+        if (launcherTheme != null) {
+            launcherTheme.setOnPreferenceChangeListener((preference, newValue) -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+
+                builder.setTitle(getString(R.string.zh_tip));
+                builder.setMessage(getString(R.string.zh_setting_reboot_tip));
+                builder.setPositiveButton(getString(R.string.zh_help_ok), null);
+
+                builder.show();
+
+                return true;
+            });
+        }
     }
 }
