762e1e71c5312ef684d9246ab54963e07e510fee
movte <212824502@qq.com>
2024-04-30 21:34:09 +0800

    调整[浅色模式]：调整界面

--- app_pojav_zh/src/main/java/com/kdt/mcgui/MineButton.java
+++ app_pojav_zh/src/main/java/com/kdt/mcgui/MineButton.java
@@ -1,7 +1,6 @@
 package com.kdt.mcgui;
 
 import android.content.*;
-import android.graphics.*;
 import android.util.*;
 
 import androidx.core.content.res.ResourcesCompat;
@@ -23,6 +22,7 @@ public class MineButton extends androidx.appcompat.widget.AppCompatButton {
 		setTypeface(ResourcesCompat.getFont(getContext(), R.font.noto_sans_bold));
 		setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.mine_button_background, null));
 		setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen._13ssp));
+		setTextColor(getResources().getColor(R.color.primary_text));
 	}
 
 }
