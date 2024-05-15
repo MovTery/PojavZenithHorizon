4592f4dbbbef74ce4843a877459a3adc0f21dfcd
movte <212824502@qq.com>
2024-05-16 00:13:22 +0800

    更新[关于界面]：新增“贡献者与鸣谢”栏目 & 优化[文件管理]：优化排序逻辑

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/fragments/AboutFragment.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/fragments/AboutFragment.java
@@ -5,10 +5,9 @@ import static net.kdt.pojavlaunch.PojavZHTools.getVersionCode;
 import static net.kdt.pojavlaunch.PojavZHTools.getVersionName;
 import static net.kdt.pojavlaunch.PojavZHTools.getVersionStatus;
 
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,15 +15,25 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.movtery.about.AboutItemBean;
+import com.movtery.about.AboutRecyclerAdapter;
+import com.movtery.filelist.SpacesItemDecoration;
 
 import net.kdt.pojavlaunch.PojavZHTools;
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AboutFragment extends Fragment {
     public static final String TAG = "AboutFragment";
+    private final List<AboutItemBean> mData = new ArrayList<>();
     private Button mReturnButton, mGithubButton, mPojavLauncherButton, mLicenseButton;
-    private TextView mContributors1, mContributors2;
+    private RecyclerView mAboutRecyclerView;
 
     public AboutFragment() {
         super(R.layout.fragment_about);
@@ -38,6 +47,7 @@ public class AboutFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         bindViews(view);
+        loadAboutData(requireContext().getResources());
 
         mReturnButton.setOnClickListener(v -> PojavZHTools.onBackPressed(requireActivity()));
         mGithubButton.setOnClickListener(v -> Tools.openURL(requireActivity(), Tools.URL_HOME));
@@ -45,8 +55,12 @@ public class AboutFragment extends Fragment {
         mPojavLauncherButton.setOnClickListener(v -> Tools.openURL(requireActivity(), "https://github.com/PojavLauncherTeam/PojavLauncher"));
         mLicenseButton.setOnClickListener(v -> Tools.openURL(requireActivity(), "https://www.gnu.org/licenses/gpl-3.0.html"));
 
-        mContributors1.setOnClickListener(v -> Tools.openURL(requireActivity(), "https://space.bilibili.com/2008204513"));
-        mContributors2.setOnClickListener(v -> Tools.openURL(requireActivity(), "https://space.bilibili.com/1412062866"));
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(requireContext());
+        mAboutRecyclerView.setLayoutManager(layoutManager);
+
+        AboutRecyclerAdapter adapter = new AboutRecyclerAdapter(this.mData);
+        mAboutRecyclerView.addItemDecoration(new SpacesItemDecoration(0, 0, 0, (int) Tools.dpToPx(8)));
+        mAboutRecyclerView.setAdapter(adapter);
     }
 
     private void bindViews(@NonNull View view) {
@@ -54,21 +68,12 @@ public class AboutFragment extends Fragment {
         mGithubButton = view.findViewById(R.id.zh_about_github_button);
         mPojavLauncherButton = view.findViewById(R.id.zh_about_pojavlauncher_button);
         mLicenseButton = view.findViewById(R.id.zh_about_license_button);
+        mAboutRecyclerView = view.findViewById(R.id.zh_about_about_recycler);
 
-        mContributors1 = view.findViewById(R.id.zh_about_contributors1);
-        mContributors2 = view.findViewById(R.id.zh_about_contributors2);
         TextView mVersionName = view.findViewById(R.id.zh_about_version_name);
         TextView mVersionCode = view.findViewById(R.id.zh_about_version_code);
         TextView mLastUpdateTime = view.findViewById(R.id.zh_about_last_update_time);
         TextView mVersionStatus = view.findViewById(R.id.zh_about_version_status);
-
-        SpannableString spannableString1 = new SpannableString(mContributors1.getText().toString());
-        spannableString1.setSpan(new UnderlineSpan(), 0, spannableString1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        mContributors1.setText(spannableString1);
-
-        SpannableString spannableString2 = new SpannableString(mContributors2.getText().toString());
-        spannableString2.setSpan(new UnderlineSpan(), 0, spannableString2.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        mContributors2.setText(spannableString2);
 
         //软件信息
         String versionName = getString(R.string.zh_about_version_name) + getVersionName(requireContext());
@@ -79,6 +84,27 @@ public class AboutFragment extends Fragment {
         mLastUpdateTime.setText(lastUpdateTime);
         String versionStatus = getString(R.string.zh_about_version_status) + getVersionStatus(requireContext());
         mVersionStatus.setText(versionStatus);
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private void loadAboutData(Resources resources) {
+        this.mData.add(new AboutItemBean(
+                resources.getDrawable(R.drawable.image_about_movtery),
+                "墨北MovTery",
+                getString(R.string.zh_about_movtery_desc),
+                new AboutItemBean.AboutItemButtonBean(requireActivity(), getString(R.string.zh_about_access_space), "https://space.bilibili.com/2008204513")));
+
+        this.mData.add(new AboutItemBean(
+                resources.getDrawable(R.drawable.image_about_verafirefly),
+                "Vera-Firefly",
+                getString(R.string.zh_about_verafirefly_desc),
+                new AboutItemBean.AboutItemButtonBean(requireActivity(), getString(R.string.zh_about_access_space), "https://space.bilibili.com/1412062866")));
+
+        this.mData.add(new AboutItemBean(
+                resources.getDrawable(R.drawable.image_about_lingmuqiuzhu),
+                "柃木秋竹",
+                getString(R.string.zh_about_lingmuqiuzhu_desc),
+                null));
     }
 }
 
