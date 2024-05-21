bc221b7e9a8d532c8d88a096d16f0337577ca28a
movte <212824502@qq.com>
2024-05-21 18:37:35 +0800

    更新[Mod下载]：支持多选Mod加载器筛选

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/fragments/SearchModFragment.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/fragments/SearchModFragment.java
@@ -5,39 +5,27 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.core.math.MathUtils;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.movtery.utils.ModLoaderList;
-import com.movtery.utils.SearchModSort;
-import com.movtery.versionlist.VersionSelectedListener;
-
 import net.kdt.pojavlaunch.PojavZHTools;
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
-import net.kdt.pojavlaunch.dialog.SelectVersionDialog;
+import net.kdt.pojavlaunch.dialog.ModFitersDialog;
 import net.kdt.pojavlaunch.modloaders.modpacks.ModItemAdapter;
 import net.kdt.pojavlaunch.modloaders.modpacks.api.CommonApi;
 import net.kdt.pojavlaunch.modloaders.modpacks.api.ModpackApi;
 import net.kdt.pojavlaunch.modloaders.modpacks.models.SearchFilters;
 import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SearchModFragment extends Fragment implements ModItemAdapter.SearchResultCallback {
 
@@ -167,91 +155,8 @@ public class SearchModFragment extends Fragment implements ModItemAdapter.Search
     }
 
     private void displayFilterDialog() {
-        AlertDialog dialog = new AlertDialog.Builder(requireContext())
-                .setView(R.layout.dialog_mod_filters)
-                .create();
-
-        // setup the view behavior
-        dialog.setOnShowListener(dialogInterface -> {
-            TextView mSelectedVersion = dialog.findViewById(R.id.search_mod_selected_mc_version_textview);
-            Button mSelectVersionButton = dialog.findViewById(R.id.search_mod_mc_version_button);
-            Spinner mSortBy = dialog.findViewById(R.id.zh_search_mod_sort);
-            Spinner mModloader = dialog.findViewById(R.id.zh_search_mod_modloader);
-            Button mApplyButton = dialog.findViewById(R.id.search_mod_apply_filters);
-
-            assert mSelectVersionButton != null;
-            assert mSelectedVersion != null;
-            assert mApplyButton != null;
-
-            // 打开版本选择弹窗
-            mSelectVersionButton.setOnClickListener(v -> {
-                SelectVersionDialog selectVersionDialog = new SelectVersionDialog(requireContext());
-                selectVersionDialog.setOnVersionSelectedListener(new VersionSelectedListener() {
-                    @Override
-                    public void onVersionSelected(String version) {
-                        mSelectedVersion.setText(version);
-                        selectVersionDialog.dismiss();
-                    }
-                });
-
-                selectVersionDialog.show();
-            });
-
-            // Apply visually all the current settings
-            mSelectedVersion.setText(mSearchFilters.mcVersion);
-
-            List<String> sortList = new ArrayList<>(SearchModSort.getIndexList());
-            if (mSortBy != null) {
-                mSortBy.setAdapter(new ArrayAdapter<>(requireContext(), R.layout.item_simple_list_1, sortList));
-                //默认选中筛选器设置的排序索引
-                mSortBy.setSelection(mSearchFilters.sort);
-
-                mSortBy.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        mSearchFilters.sort = position;
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                    }
-                });
-            }
-
-            List<String> modloaderList = new ArrayList<>(ModLoaderList.getModloaderList());
-            if (mModloader != null) {
-                mModloader.setAdapter(new ArrayAdapter<>(requireContext(), R.layout.item_simple_list_1, modloaderList));
-                //默认选中筛选器设置的Mod加载器
-                mModloader.setSelection(mSearchFilters.modloaders.isEmpty() ? 0 : ModLoaderList.getModloaderId(mSearchFilters.modloaders.get(0)));
-
-                mModloader.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        List<String> modloader = new ArrayList<>();
-                        String string = parent.getItemAtPosition(position).toString();
-                        if (string.equals(getString(R.string.zh_unknown))) {
-                            mSearchFilters.modloaders = modloader;
-                            return;
-                        }
-                        modloader.add(string);
-                        mSearchFilters.modloaders = modloader;
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                    }
-                });
-            }
-
-            // Apply the new settings
-            mApplyButton.setOnClickListener(v -> {
-                mSearchFilters.mcVersion = mSelectedVersion.getText().toString();
-                searchMods(mSearchEditText.getText().toString());
-                dialogInterface.dismiss();
-            });
-        });
-
-
-        dialog.show();
+        ModFitersDialog modFitersDialog = new ModFitersDialog(requireContext(), mSearchFilters);
+        modFitersDialog.setOnApplyButtonClickListener(() -> searchMods(mSearchEditText.getText().toString()));
+        modFitersDialog.show();
     }
 }
