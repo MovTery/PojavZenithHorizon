14ddfe0eb45cb0190918f120944027805673b8b2
movte <212824502@qq.com>
2024-05-31 03:28:27 +0800

    更新[下载Mod]：Mod版本显示版本发行状态

--- app_pojav_zh/src/main/java/com/movtery/ui/subassembly/downloadmod/ModVersionAdapter.java
+++ app_pojav_zh/src/main/java/com/movtery/ui/subassembly/downloadmod/ModVersionAdapter.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -63,17 +64,22 @@ public class ModVersionAdapter extends RecyclerView.Adapter<ModVersionAdapter.In
 
     public class InnerHolder extends RecyclerView.ViewHolder {
         private final View mainView;
-        private final TextView mTitle, mDownloadCount, mModloaders;
+        private final ImageView mImageView;
+        private final TextView mTitle, mDownloadCount, mModloaders, mReleaseType;
 
         public InnerHolder(@NonNull View itemView) {
             super(itemView);
             mainView = itemView;
+            mImageView = itemView.findViewById(R.id.mod_download_imageview);
             mTitle = itemView.findViewById(R.id.mod_title_textview);
             mDownloadCount = itemView.findViewById(R.id.zh_mod_download_count_textview);
             mModloaders = itemView.findViewById(R.id.zh_mod_modloader_textview);
+            mReleaseType = itemView.findViewById(R.id.zh_mod_release_type_textview);
         }
 
         public void setData(ModVersionGroup.ModVersionItem modVersionItem) {
+            mImageView.setImageResource(getDownloadType(modVersionItem.getVersionType()));
+
             mTitle.setText(modVersionItem.getTitle());
 
             String downloadCountText = mainView.getContext().getString(R.string.zh_profile_mods_information_download_count) + " " +
@@ -87,6 +93,8 @@ public class ModVersionAdapter extends RecyclerView.Adapter<ModVersionAdapter.In
                 modloaderText += mainView.getContext().getString(R.string.zh_unknown);
             }
             mModloaders.setText(modloaderText);
+
+            mReleaseType.setText(getDownloadTypeText(modVersionItem.getVersionType()));
 
             mainView.setOnClickListener(v -> {
                 if (mTasksRunning) {
@@ -106,6 +114,37 @@ public class ModVersionAdapter extends RecyclerView.Adapter<ModVersionAdapter.In
 
                 mod.api.handleInstallation(mainView.getContext(), mod.isModpack, mod.modsPath, modDetail, modVersionItem);
             });
+        }
+
+        private int getDownloadType(VersionType.VersionTypeEnum versionType) {
+            switch (versionType) {
+                case BETA:
+                    return R.drawable.ic_download_beta;
+                case ALPHA:
+                    return R.drawable.ic_download_alpha;
+                default:
+                case RELEASE:
+                    return R.drawable.ic_download_release;
+            }
+        }
+
+        private String getDownloadTypeText(VersionType.VersionTypeEnum versionType) {
+            String text = mainView.getContext().getString(R.string.zh_profile_mods_information_release_type) + " ";
+            switch (versionType) {
+                case RELEASE:
+                    text += mainView.getContext().getString(R.string.zh_profile_mods_information_release_type_release);
+                    break;
+                case BETA:
+                    text += mainView.getContext().getString(R.string.zh_profile_mods_information_release_type_beta);
+                    break;
+                case ALPHA:
+                    text += mainView.getContext().getString(R.string.zh_profile_mods_information_release_type_alpha);
+                    break;
+                default:
+                    text += mainView.getContext().getString(R.string.zh_unknown);
+                    break;
+            }
+            return text;
         }
     }
 }
