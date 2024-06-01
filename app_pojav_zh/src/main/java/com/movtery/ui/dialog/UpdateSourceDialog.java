bf5588d4f43efea2d640564710d2badc1edf08c4
movte <212824502@qq.com>
2024-06-02 00:28:30 +0800

    更新[检查更新]：新增ghproxy镜像源

---
+++ app_pojav_zh/src/main/java/com/movtery/ui/dialog/UpdateSourceDialog.java
@@ -0,0 +1,50 @@
+package com.movtery.ui.dialog;
+
+import static net.kdt.pojavlaunch.Tools.runOnUiThread;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.movtery.feature.UpdateLauncher;
+
+import net.kdt.pojavlaunch.R;
+
+public class UpdateSourceDialog extends Dialog {
+    private final String tagName, fileSize;
+
+    public UpdateSourceDialog(@NonNull Context context, String tagName, String fileSize) {
+        super(context);
+
+        this.tagName = tagName;
+        this.fileSize = fileSize;
+
+        this.setCancelable(true);
+        this.setContentView(R.layout.dialog_update_source);
+
+        init();
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private void init() {
+        Button mGithubRelease = findViewById(R.id.zh_update_github_release);
+        Button mGhproxy = findViewById(R.id.zh_update_ghproxy);
+
+        mGithubRelease.setOnClickListener(view -> {
+            runOnUiThread(() -> Toast.makeText(getContext(), getContext().getString(R.string.zh_update_downloading_tip, "Github Release"), Toast.LENGTH_SHORT).show());
+            UpdateLauncher updateLauncher = new UpdateLauncher(getContext(), tagName, fileSize, UpdateLauncher.UpdateSource.GITHUB_RELEASE);
+            updateLauncher.start();
+            UpdateSourceDialog.this.dismiss();
+        });
+        mGhproxy.setOnClickListener(view -> {
+            runOnUiThread(() -> Toast.makeText(getContext(), getContext().getString(R.string.zh_update_downloading_tip, getContext().getString(R.string.zh_update_update_source_ghproxy)), Toast.LENGTH_SHORT).show());
+            UpdateLauncher updateLauncher = new UpdateLauncher(getContext(), tagName, fileSize, UpdateLauncher.UpdateSource.GHPROXY);
+            updateLauncher.start();
+            UpdateSourceDialog.this.dismiss();
+        });
+    }
+}
