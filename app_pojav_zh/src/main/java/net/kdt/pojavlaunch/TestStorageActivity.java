3b84227c8fbde567050caf15e651812655934cd0
movte <212824502@qq.com>
2024-06-05 14:41:34 +0800

    更新：请求“管理所有文件”的权限

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/TestStorageActivity.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/TestStorageActivity.java
@@ -2,62 +2,105 @@ package net.kdt.pojavlaunch;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.os.Environment;
+import android.provider.Settings;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import net.kdt.pojavlaunch.tasks.AsyncAssetManager;
 
 public class TestStorageActivity extends Activity {
-    private final int REQUEST_STORAGE_REQUEST_CODE = 1;
+    private static final int REQUEST_CODE_PERMISSIONS = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Tools.setFullscreen(this, true);
-        if(Build.VERSION.SDK_INT < 29 && !isStorageAllowed(this)) requestStoragePermission();
-        else exit();
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            handlePermissionsForAndroid11AndAbove();
+        } else {
+            handlePermissionsForAndroid10AndBelow();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            checkPermissions();
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_STORAGE_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                exit();
-            } else {
-                Toast.makeText(this, R.string.toast_permission_denied, Toast.LENGTH_LONG).show();
-                requestStoragePermission();
-            }
+        if (requestCode == REQUEST_CODE_PERMISSIONS && hasAllPermissionsGranted(grantResults)) {
+            exit();
+        } else {
+            checkPermissions();
         }
     }
 
-    public static boolean isStorageAllowed(Context context) {
-        //Getting the permission status
-        int result1 = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int result2 = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE);
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private void handlePermissionsForAndroid11AndAbove() {
+        if (!Environment.isExternalStorageManager()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.zh_tip);
+            builder.setMessage(R.string.zh_permissions_manage_external_storage);
+            builder.setCancelable(false);
+            builder.setPositiveButton(R.string.zh_confirm, (dialog, which) -> {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE_PERMISSIONS);
+            });
 
-
-        //If permission is granted returning true
-        return result1 == PackageManager.PERMISSION_GRANTED &&
-                result2 == PackageManager.PERMISSION_GRANTED;
+            builder.show();
+        } else {
+            exit();
+        }
     }
 
-    private void requestStoragePermission() {
+    private void handlePermissionsForAndroid10AndBelow() {
+        if (!hasStoragePermissions()) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, REQUEST_CODE_PERMISSIONS);
+        } else {
+            exit();
+        }
+    }
 
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_STORAGE_REQUEST_CODE);
+    private boolean hasStoragePermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean hasAllPermissionsGranted(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private void exit() {
-        if(!Tools.checkStorageRoot(this)) {
+        if (!Tools.checkStorageRoot(this)) {
             startActivity(new Intent(this, MissingStorageActivity.class));
             return;
         }
@@ -65,7 +108,7 @@ public class TestStorageActivity extends Activity {
         AsyncAssetManager.unpackComponents(this);
         AsyncAssetManager.unpackSingleFiles(this);
 
-        Intent intent =  new Intent(this, LauncherActivity.class);
+        Intent intent = new Intent(this, LauncherActivity.class);
         startActivity(intent);
         finish();
     }
