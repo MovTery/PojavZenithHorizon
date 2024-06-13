9fc206c55cc9662cf9ff7e08159bd9ffee091b8d
movte <212824502@qq.com>
2024-06-13 09:31:51 +0800

    优化[下载Mod]：使用切换子父级，避免计算高度带来的性能开销

---
+++ app_pojav_zh/src/main/java/com/movtery/ui/subassembly/twolevellist/TwoLevelListAdapter.java
@@ -0,0 +1,71 @@
+package com.movtery.ui.subassembly.twolevellist;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import net.kdt.pojavlaunch.R;
+
+import java.util.List;
+
+public class TwoLevelListAdapter extends RecyclerView.Adapter<TwoLevelListAdapter.InnerHolder> {
+    private final TwoLevelListFragment fragment;
+    private final List<TwoLevelListItemBean> mData;
+
+    public TwoLevelListAdapter(TwoLevelListFragment fragment, List<TwoLevelListItemBean> mData) {
+        this.fragment = fragment;
+        this.mData = mData;
+    }
+
+
+    @NonNull
+    @Override
+    public TwoLevelListAdapter.InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mod_download, parent, false);
+        return new InnerHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TwoLevelListAdapter.InnerHolder holder, int position) {
+        holder.setData(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData != null ? mData.size() : 0;
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void updateData(List<TwoLevelListItemBean> newData) {
+        mData.clear();
+        mData.addAll(newData);
+        super.notifyDataSetChanged();
+    }
+
+    public List<TwoLevelListItemBean> getData() {
+        return mData;
+    }
+
+    public class InnerHolder extends RecyclerView.ViewHolder {
+        private final View mainView;
+        private final TextView versionId;
+
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+            mainView = itemView;
+            versionId = itemView.findViewById(R.id.mod_version_id);
+        }
+
+        public void setData(TwoLevelListItemBean twoLevelListItemBean) {
+            mainView.setOnClickListener(v ->
+                    fragment.switchToChild(twoLevelListItemBean.getAdapter(), twoLevelListItemBean.getTitle()));
+
+            versionId.setText(twoLevelListItemBean.getTitle());
+        }
+    }
+}
---
+++ app_pojav_zh/src/main/java/com/movtery/ui/subassembly/twolevellist/TwoLevelListFragment.java
@@ -0,0 +1,147 @@
+package com.movtery.ui.subassembly.twolevellist;
+
+import static net.kdt.pojavlaunch.prefs.LauncherPreferences.PREF_ANIMATION;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.movtery.utils.PojavZHTools;
+
+import net.kdt.pojavlaunch.R;
+
+import java.util.concurrent.Future;
+
+public abstract class TwoLevelListFragment extends Fragment {
+    private RecyclerView.Adapter<?> parentAdapter = null;
+    private RecyclerView mRecyclerView;
+    private ProgressBar mProgressBar;
+    private TextView mLoadingText, mNameText, mSelectTitle;
+    private ImageView mIcon;
+    private Button mReturnButton, mRefreshButton;
+    private CheckBox mReleaseCheckBox;
+    private Future<?> currentTask;
+
+    public TwoLevelListFragment() {
+        super(R.layout.fragment_mod_download);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        bindViews(view);
+        init();
+    }
+
+    protected void init() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(requireContext());
+        if (PREF_ANIMATION) mRecyclerView.setLayoutAnimation(new LayoutAnimationController(AnimationUtils.loadAnimation(requireContext(), R.anim.fade_downwards)));
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRefreshButton.setOnClickListener(v -> refreshTask());
+        mReleaseCheckBox.setOnClickListener(v -> refreshTask());
+        mReturnButton.setOnClickListener(v -> {
+            if (parentAdapter != null) {
+                setSelectTitleVisible(false);
+                mRecyclerView.setAdapter(parentAdapter);
+                mRecyclerView.scheduleLayoutAnimation();
+                parentAdapter = null;
+            } else {
+                PojavZHTools.onBackPressed(requireActivity());
+            }
+        });
+
+        refreshTask();
+    }
+
+    @Override
+    public void onDestroy() {
+        cancelTask();
+        super.onDestroy();
+    }
+
+    private void setSelectTitleVisible(boolean visible) {
+        if (PREF_ANIMATION) {
+            PojavZHTools.fadeAnim(mSelectTitle, 0, visible ? 0 : 1, visible ? 1 : 0, 200,
+                    () -> mSelectTitle.setVisibility(visible ? View.VISIBLE : View.GONE));
+        } else {
+            mSelectTitle.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    private void cancelTask() {
+        if (currentTask != null && !currentTask.isDone()) {
+            currentTask.cancel(true);
+        }
+    }
+
+    private void refreshTask() {
+        currentTask = refresh();
+    }
+
+    protected abstract Future<?> refresh();
+
+    protected void componentProcessing(boolean state) {
+        mProgressBar.setVisibility(state ? View.VISIBLE : View.GONE);
+        mLoadingText.setVisibility(state ? View.VISIBLE : View.GONE);
+        mRecyclerView.setVisibility(state ? View.GONE : View.VISIBLE);
+
+        mRefreshButton.setClickable(!state);
+        mReleaseCheckBox.setClickable(!state);
+    }
+
+    private void bindViews(View view) {
+        mRecyclerView = view.findViewById(R.id.zh_mod);
+        mProgressBar = view.findViewById(R.id.zh_mod_loading);
+        mLoadingText = view.findViewById(R.id.zh_mod_loading_text);
+        mIcon = view.findViewById(R.id.zh_mod_icon);
+        mNameText = view.findViewById(R.id.zh_mod_name);
+        mSelectTitle = view.findViewById(R.id.zh_select_title);
+        mReturnButton = view.findViewById(R.id.zh_mod_return_button);
+        mRefreshButton = view.findViewById(R.id.zh_mod_refresh_button);
+        mReleaseCheckBox = view.findViewById(R.id.zh_mod_release_version);
+    }
+
+    protected void setNameText(String nameText) {
+        this.mNameText.setText(nameText);
+    }
+
+    protected void setIcon(Drawable icon) {
+        this.mIcon.setImageDrawable(icon);
+    }
+
+    public RecyclerView getRecyclerView() {
+        return mRecyclerView;
+    }
+
+    public CheckBox getReleaseCheckBox() {
+        return mReleaseCheckBox;
+    }
+
+    public Future<?> getCurrentTask() {
+        return currentTask;
+    }
+
+    public void switchToChild(RecyclerView.Adapter<?> adapter, String title) {
+        if (adapter != null) {
+            //保存父级，设置选中的标题文本，切换至子级
+            parentAdapter = mRecyclerView.getAdapter();
+            mSelectTitle.setText(title);
+            setSelectTitleVisible(true);
+            mRecyclerView.setAdapter(adapter);
+            mRecyclerView.scheduleLayoutAnimation();
+        }
+    }
+}
