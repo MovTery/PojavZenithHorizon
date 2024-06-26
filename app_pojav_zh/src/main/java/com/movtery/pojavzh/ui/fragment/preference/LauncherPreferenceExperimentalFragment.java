c999c9bd314da93db43dcdaa515c203fb02d90d9
movte <212824502@qq.com>
2024-06-27 00:36:39 +0800

    调整[设置]：调整实验性设置

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/ui/fragment/preference/LauncherPreferenceExperimentalFragment.java
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/ui/fragment/preference/LauncherPreferenceExperimentalFragment.java
@@ -1,8 +1,9 @@
-package net.kdt.pojavlaunch.prefs.screens;
+package com.movtery.pojavzh.ui.fragment.preference;
 
 import android.os.Bundle;
 
 import net.kdt.pojavlaunch.R;
+import net.kdt.pojavlaunch.prefs.screens.LauncherPreferenceFragment;
 
 public class LauncherPreferenceExperimentalFragment extends LauncherPreferenceFragment {
     public static final String TAG = "LauncherPreferenceExperimentalFragment";
