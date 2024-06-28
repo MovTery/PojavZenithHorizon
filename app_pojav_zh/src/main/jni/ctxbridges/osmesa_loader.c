a5cf4866442846d6fe8af140b5fcd6ef680687f9
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-06-28 15:37:30 +0800

    功能[渲染器]: 增加选择Mesa版本功能 & 重构渲染器选择方法 & 添加渲染器实验性设置

--- app_pojav_zh/src/main/jni/ctxbridges/osmesa_loader.c
+++ app_pojav_zh/src/main/jni/ctxbridges/osmesa_loader.c
@@ -1,5 +1,5 @@
 //
-// Modifiled by Vera-Firefly on 29.03.2024.
+// Modifiled by Vera-Firefly on 19.03.2024.
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,33 +13,39 @@ GLboolean (*OSMesaMakeCurrent_p) (OSMesaContext ctx, void *buffer, GLenum type,
 OSMesaContext (*OSMesaGetCurrentContext_p) (void);
 OSMesaContext  (*OSMesaCreateContext_p) (GLenum format, OSMesaContext sharelist);
 void (*OSMesaDestroyContext_p) (OSMesaContext ctx);
+void (*OSMesaFlushFrontbuffer_p) ();
 void (*OSMesaPixelStore_p) ( GLint pname, GLint value );
 GLubyte* (*glGetString_p) (GLenum name);
 void (*glFinish_p) (void);
 void (*glClearColor_p) (GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
 void (*glClear_p) (GLbitfield mask);
 void (*glReadPixels_p) (GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, void * data);
+void (*glReadBuffer_p) (GLenum mode);
 
 void dlsym_OSMesa() {
     char* main_path = NULL;
     char* mesa_library = getenv("MESA_LIBRARY");
-    if(pojav_environ->config_renderer == RENDERER_VK_ZINK || pojav_environ->config_renderer == RENDERER_VIRGL) {
+    if(pojav_environ->config_renderer == RENDERER_VK_ZINK
+    || pojav_environ->config_renderer == RENDERER_VK_ZINK_PREF
+    || pojav_environ->config_renderer == RENDERER_VIRGL) {
         if(asprintf(&main_path, "%s/%s", getenv("POJAV_NATIVEDIR"), mesa_library) == -1) {
             abort();
         }
-        free(main_path);
     }
     void* dl_handle = NULL;
     dl_handle = dlopen(main_path, RTLD_GLOBAL);
+    free(main_path);
     if(dl_handle == NULL) abort();
     OSMesaMakeCurrent_p = dlsym(dl_handle, "OSMesaMakeCurrent");
     OSMesaGetCurrentContext_p = dlsym(dl_handle,"OSMesaGetCurrentContext");
     OSMesaCreateContext_p = dlsym(dl_handle, "OSMesaCreateContext");
     OSMesaDestroyContext_p = dlsym(dl_handle, "OSMesaDestroyContext");
+    OSMesaFlushFrontbuffer_p = dlsym(dl_handle, "OSMesaFlushFrontbuffer");
     OSMesaPixelStore_p = dlsym(dl_handle,"OSMesaPixelStore");
     glGetString_p = dlsym(dl_handle,"glGetString");
     glClearColor_p = dlsym(dl_handle, "glClearColor");
     glClear_p = dlsym(dl_handle,"glClear");
     glFinish_p = dlsym(dl_handle,"glFinish");
     glReadPixels_p = dlsym(dl_handle,"glReadPixels");
+    glReadBuffer_p = dlsym(dl_handle, "glReadBuffer");
 }
