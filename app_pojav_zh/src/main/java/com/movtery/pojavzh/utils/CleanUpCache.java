b67c7f58ba416db188b5fe3eb493807d829ac4b2
movte <212824502@qq.com>
2024-07-06 20:22:54 +0800

    更新[清理缓存]：可清理应用外部缓存路径

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/CleanUpCache.java
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/CleanUpCache.java
@@ -11,6 +11,9 @@ import net.kdt.pojavlaunch.Tools;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CleanUpCache {
     private static boolean isCleaning = false;
@@ -21,37 +24,44 @@ public class CleanUpCache {
         long totalSize = 0;
         int fileCount = 0;
         try {
-            File[] files = Tools.DIR_CACHE.listFiles();
+            List<File> list = getList(Tools.DIR_CACHE.listFiles(), ZHTools.DIR_APP_CACHE.listFiles());
 
-            if (files != null) {
-                for (File file : files) {
-                    if (file.getName().equals("user_icon")) continue;
+            for (File file : list) {
+                if (file.getName().equals("user_icon")) continue;
 
-                    ++fileCount;
+                ++fileCount;
 
-                    if (file.isDirectory()) {
-                        totalSize += FileUtils.sizeOfDirectory(file);
-                    } else {
-                        totalSize += FileUtils.sizeOf(file);
-                    }
-
-                    FileUtils.deleteQuietly(file);
+                if (file.isDirectory()) {
+                    totalSize += FileUtils.sizeOfDirectory(file);
+                } else {
+                    totalSize += FileUtils.sizeOf(file);
                 }
 
-                int finalFileCount = fileCount;
-                long finalTotalSize = totalSize;
-                runOnUiThread(() -> {
-                    if (finalFileCount != 0) {
-                        Toast.makeText(context, context.getString(R.string.zh_clear_up_cache_clean_up, ZHTools.formatFileSize(finalTotalSize)), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(context, context.getString(R.string.zh_clear_up_cache_not_found), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            } else {
-                runOnUiThread(() -> Toast.makeText(context, context.getString(R.string.zh_clear_up_cache_not_found), Toast.LENGTH_SHORT).show());
+                FileUtils.deleteQuietly(file);
             }
+
+            int finalFileCount = fileCount;
+            long finalTotalSize = totalSize;
+            runOnUiThread(() -> {
+                if (finalFileCount != 0) {
+                    Toast.makeText(context, context.getString(R.string.zh_clear_up_cache_clean_up, ZHTools.formatFileSize(finalTotalSize)), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, context.getString(R.string.zh_clear_up_cache_not_found), Toast.LENGTH_SHORT).show();
+                }
+            });
         } finally {
             isCleaning = false;
         }
+    }
+
+    private static List<File> getList(File[]... filesArray) {
+        List<File> filesList = new ArrayList<>();
+        for (File[] fileArray : filesArray) {
+            if (fileArray != null) {
+                filesList.addAll(Arrays.asList(fileArray));
+            }
+        }
+
+        return filesList;
     }
 }
