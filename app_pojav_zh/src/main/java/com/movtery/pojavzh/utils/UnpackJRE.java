fd61fedb0b193e4ecc7959cc037a5caa8fe3b841
movte <212824502@qq.com>
2024-07-10 04:13:50 +0800

    调整[解压JRE]：调整代码

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/UnpackJRE.java
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/UnpackJRE.java
@@ -7,11 +7,13 @@ import android.util.Log;
 
 import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.multirt.MultiRTUtils;
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 
 import java.io.IOException;
 
 public class UnpackJRE {
     public static void unpackAllJre(AssetManager assetManager) {
+        checkInternalRuntime(assetManager, InternalRuntime.JRE_8);
         checkInternalRuntime(assetManager, InternalRuntime.JRE_17);
         checkInternalRuntime(assetManager, InternalRuntime.JRE_21);
     }
@@ -35,6 +37,11 @@ public class UnpackJRE {
                     assetManager.open(internalRuntime.path + "/universal.tar.xz"),
                     assetManager.open(internalRuntime.path + "/bin-" + archAsString(Tools.DEVICE_ARCHITECTURE) + ".tar.xz"),
                     internalRuntime.name, version);
+            //设置默认运行环境
+            if (internalRuntime == InternalRuntime.JRE_8 && LauncherPreferences.PREF_DEFAULT_RUNTIME.isEmpty()) {
+                LauncherPreferences.PREF_DEFAULT_RUNTIME = internalRuntime.name;
+                LauncherPreferences.DEFAULT_PREF.edit().putString("defaultRuntime", LauncherPreferences.PREF_DEFAULT_RUNTIME).apply();
+            }
             MultiRTUtils.postPrepare(internalRuntime.name);
         } catch (IOException e) {
             Log.e("UnpackJREAuto", "Internal JRE unpack failed", e);
@@ -42,6 +49,7 @@ public class UnpackJRE {
     }
 
     private enum InternalRuntime {
+        JRE_8(8, "Internal-8", "components/jre-8"),
         JRE_17(17, "Internal-17", "components/jre-17"),
         JRE_21(21, "Internal-21", "components/jre-21");
         public final int majorVersion;
