16188c9445c8fcdf5a755aa00077360978f28947
movte <212824502@qq.com>
2024-07-10 16:22:56 +0800

    调整[项目]：删除未使用的文件

--- scripts/languagelist_updater.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set thisdir = "%~dp0"
-set langfile = %thisdir%\..\app_pojav_zh\src\main\assets\language_list.txt
-
-del %langfile%
-dir %thisdir%\..\app_pojav_zh\src\main\res\values-* /s /b > %langfile%
-
