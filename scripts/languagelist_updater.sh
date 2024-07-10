2cad5a60912175659a2a5d9f86cf9e19c3f05691
movte <212824502@qq.com>
2024-07-10 16:22:56 +0800

    调整[项目]：删除未使用的文件

--- scripts/languagelist_updater.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-THISDIR=`dirname $0`
-LANGFILE=$THISDIR/../app_pojav_zh/src/main/assets/language_list.txt
-
-rm -f $LANGFILE
-echo $THISDIR/../app_pojav_zh/src/main/res/values-* | xargs -- basename -a > $LANGFILE
-
