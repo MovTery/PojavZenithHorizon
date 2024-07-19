86989ed7e160eae0515a610ee938479c26b3c751
movte <212824502@qq.com>
2024-07-19 14:18:39 +0800

    更新[关于界面]：展示赞助者鸣谢(测试中)

---
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/http/CallUtils.java
@@ -0,0 +1,47 @@
+package com.movtery.pojavzh.utils.http;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class CallUtils {
+    private final CallbackListener listener;
+    private final String url;
+    private final String token;
+
+    public CallUtils(CallbackListener listener, String url, String token) {
+        this.listener = listener;
+        this.url = url;
+        this.token = token;
+    }
+
+    public void start() {
+        OkHttpClient client = new OkHttpClient();
+        Request.Builder url = new Request.Builder().url(this.url);
+        if (token != null) {
+            url.header("Authorization", "token " + token);
+        }
+        Request request = url.build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                listener.onFailure(call, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                listener.onResponse(call, response);
+            }
+        });
+    }
+
+    public interface CallbackListener {
+        void onFailure(Call call, IOException e);
+        void onResponse(Call call, Response response) throws IOException;
+    }
+}
