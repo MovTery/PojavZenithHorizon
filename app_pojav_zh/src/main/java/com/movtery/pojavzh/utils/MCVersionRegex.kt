65d1ad3eb1ac630eb6e14ed260d3b11b8ee6c52e
movte <212824502@qq.com>
2024-08-02 14:07:01 +0800

    调整[项目]：调整代码，清理未使用资源

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/MCVersionRegex.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/MCVersionRegex.kt
@@ -2,8 +2,7 @@ package com.movtery.pojavzh.utils
 
 import java.util.regex.Pattern
 
-class MCVersionRegex {
-    companion object{
-        @JvmStatic val RELEASE_REGEX: Pattern = Pattern.compile("^\\d+\\.\\d+\\.\\d+$|^\\d+\\.\\d+$")
-    }
+object MCVersionRegex {
+    @JvmStatic val RELEASE_REGEX: Pattern = Pattern.compile("^\\d+\\.\\d+\\.\\d+$|^\\d+\\.\\d+$")
+    @JvmStatic val SNAPSHOT_REGEX: Pattern = Pattern.compile("^\\d+[a-zA-Z]\\d+[a-zA-Z]$")
 }
