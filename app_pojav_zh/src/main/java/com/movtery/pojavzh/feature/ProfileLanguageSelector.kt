8ef0e96e983271a5cfdbff9c28246f235df8af2b
movte <212824502@qq.com>
2024-08-08 17:32:24 +0800

    调整[项目]：调整代码 & 设置游戏语言时的性能优化

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/ProfileLanguageSelector.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/ProfileLanguageSelector.kt
@@ -9,91 +9,66 @@ import net.kdt.pojavlaunch.Tools
 import net.kdt.pojavlaunch.prefs.LauncherPreferences
 import net.kdt.pojavlaunch.utils.MCOptionUtils
 import net.kdt.pojavlaunch.value.launcherprofiles.MinecraftProfile
-import java.io.IOException
 
 object ProfileLanguageSelector {
     private fun getOlderLanguage(lang: String): String {
-        val builder = StringBuilder(lang)
         val underscoreIndex = lang.indexOf('_')
-
-        if (underscoreIndex != -1) {
-            for (i in underscoreIndex until lang.length) {
-                builder.setCharAt(i, lang[i].uppercaseChar())
-            } //只将下划线后面的字符转换为大写
-        }
-
-        return builder.toString()
+        return if (underscoreIndex != -1) {
+            //只将下划线后面的字符转换为大写
+            val builder = StringBuilder(lang.substring(0, underscoreIndex + 1))
+            builder.append(lang.substring(underscoreIndex + 1).uppercase())
+            builder.toString()
+        } else lang
     }
 
     @Throws(NumberFormatException::class)
     private fun getVersion(versionId: String): Int {
-        val firstDotIndex = versionId.indexOf('.')
-        val secondDotIndex = versionId.indexOf('.', firstDotIndex + 1)
-
-        val version = if (firstDotIndex != -1) { // 官方版本
-            if (secondDotIndex == -1) versionId.substring(firstDotIndex + 1).toInt()
-            else versionId.substring(firstDotIndex + 1, secondDotIndex).toInt()
-        } else 12
-        return version
+        val versionParts = versionId.split('.')
+        return when {
+            versionParts.size > 1 -> versionParts[1].toInt()
+            else -> 12
+        }
     }
 
     private fun getLanguage(versionName: String?, rawLang: String?): String? {
         if (versionName == null || rawLang == null) return null
-        var lang: String = rawLang
-        if (rawLang == "system") lang = ZHTools.getSystemLanguage()
+        val lang = if (rawLang == "system") ZHTools.getSystemLanguage() else rawLang
 
-        val version: JMinecraftVersionList.Version
-        try {
-            version = Tools.GLOBAL_GSON.fromJson(
+        val version = runCatching {
+            Tools.GLOBAL_GSON.fromJson(
                 Tools.read("$versionsHome/$versionName/$versionName.json"),
                 JMinecraftVersionList.Version::class.java
             )
-        } catch (e: IOException) {
+        }.getOrElse { e ->
             throw RuntimeException(e)
         }
 
         val versionId = version.id
 
-        val pattern = MCVersionRegex.SNAPSHOT_REGEX
-        val matcher = pattern.matcher(versionId)
-
-        if (StringUtils.containsDot(versionId)) {
-            try {
-                val ver = getVersion(versionId)
-
-                // 1.10 -
-                if (ver < 11) {
-                    return getOlderLanguage(lang)
-                }
-
-                return lang // ? & 1.0
-            } catch (e: NumberFormatException) {
-                return lang
+        return when {
+            StringUtils.containsDot(versionId) -> { // 1.10 -
+                runCatching {
+                    val ver = getVersion(versionId)
+                    if (ver < 11) getOlderLanguage(lang) else lang
+                }.getOrDefault(lang)
             }
-        } else if (matcher.matches()) { // 快照版本 "24w09a" "16w20a"
-            try {
-                val result = StringUtils.extractNumbers(versionId, 2)
-
-                if (result[0] < 16) {
-                    return getOlderLanguage(lang)
-                } else if ((result[0] == 16) and (result[1] <= 32)) {
-                    return getOlderLanguage(lang)
-                }
-
-                return lang
-            } catch (e: NumberFormatException) {
-                return lang
+            MCVersionRegex.SNAPSHOT_REGEX.matcher(versionId).matches() -> { // 快照版本 "24w09a" "16w20a"
+                runCatching {
+                    val result = StringUtils.extractNumbers(versionId, 2)
+                    when {
+                        result[0] < 16 -> getOlderLanguage(lang)
+                        result[0] == 16 && result[1] <= 32 -> getOlderLanguage(lang)
+                        else -> lang
+                    }
+                }.getOrDefault(lang)
             }
+            else -> lang
         }
-
-        return lang
     }
 
     @JvmStatic
     fun setGameLanguage(minecraftProfile: MinecraftProfile, overridden: Boolean) {
-        if (MCOptionUtils.containsKey("lang")) {
-            if (!overridden) return
-        }
+        if (MCOptionUtils.containsKey("lang") && !overridden) return
         val language = getLanguage(minecraftProfile.lastVersionId, LauncherPreferences.PREF_GAME_LANGUAGE)
         MCOptionUtils.set("lang", language)
     }
