89cb2b8ed9d4857e4d1d22e09adf315b28825aac
movte <212824502@qq.com>
2024-08-26 01:08:34 +0800

    调整[下载Mod]：合并重复代码

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/ModTranslateManager.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/ModTranslateManager.kt
@@ -1,34 +1,3 @@
 package com.movtery.pojavzh.feature.mod.translate
 
-import com.movtery.pojavzh.feature.log.Logging
-import net.kdt.pojavlaunch.PojavApplication
-import net.kdt.pojavlaunch.Tools
-
-object ModTranslateManager {
-    private val infos: MutableList<ModTranslateInfo> = ArrayList()
-
-    init {
-        runCatching {
-            val input = PojavApplication.getContext().assets.open("data_mod.txt")
-            Utils.identificationData(input, infos)
-        }.getOrElse { e ->
-            Logging.e("ModTranslateManager", Tools.printToString(e))
-        }
-    }
-
-    /**
-     * 通过原始名称得到一个准确或还算准确的中文翻译名
-     */
-    fun searchToChinese(origin: String): String? {
-        if (infos.isEmpty()) return null
-        return Utils.searchBestMatch(infos, origin, matchBy = { it.originName }, returnBy = { it.chineseName })
-    }
-
-    /**
-     * 通过中文名称得到一个准确或还算准确的原始名
-     */
-    fun searchToOrigin(chinese: String): String? {
-        if (infos.isEmpty()) return null
-        return Utils.searchBestMatch(infos, chinese, matchBy = { it.chineseName }, returnBy = { it.originName })
-    }
-}
+object ModTranslateManager: TranslateManager("Mod", "data_mod.txt")
