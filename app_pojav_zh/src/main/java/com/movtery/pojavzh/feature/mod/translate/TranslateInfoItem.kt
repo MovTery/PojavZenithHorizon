eb4585da43401c19f2751e03dc22bdc35fcaaab4
movte <212824502@qq.com>
2024-08-26 01:08:34 +0800

    调整[下载Mod]：合并重复代码

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/TranslateInfoItem.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/TranslateInfoItem.kt
@@ -1,6 +1,6 @@
 package com.movtery.pojavzh.feature.mod.translate
 
-class ModTranslateInfo(
+class TranslateInfoItem(
     val modIndex: String,
     val chineseName: String,
     val originName: String
--- app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/Utils.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/Utils.kt
@@ -7,20 +7,20 @@ import java.util.Locale
 
 class Utils {
     companion object {
-        fun identificationData(input: InputStream, infos: MutableList<ModTranslateInfo>) {
+        fun identificationData(input: InputStream, infos: MutableList<TranslateInfoItem>) {
             val data = Tools.read(input)
 
             val lines = data.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
             for (line in lines) {
                 val parts = line.split(";;".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
                 if (parts.size == 3) {
-                    val info = ModTranslateInfo(parts[0].trim(), parts[1].trim(), parts[2].trim())
+                    val info = TranslateInfoItem(parts[0].trim(), parts[1].trim(), parts[2].trim())
                     infos.add(info)
                 }
             }
         }
 
-        fun searchBestMatch(infos: MutableList<ModTranslateInfo>, input: String, matchBy: (ModTranslateInfo) -> String, returnBy: (ModTranslateInfo) -> String): String? {
+        fun searchBestMatch(infos: MutableList<TranslateInfoItem>, input: String, matchBy: (TranslateInfoItem) -> String, returnBy: (TranslateInfoItem) -> String): String? {
             val normalizedInput = input.trim().lowercase()
             //完全匹配
             infos.firstOrNull { matchBy(it).equals(normalizedInput, ignoreCase = true) }?.let { return returnBy(it) }
