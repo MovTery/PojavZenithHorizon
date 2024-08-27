18bd22b5571b93e4b5099850eb08c14d5bc9091b
movte <212824502@qq.com>
2024-08-28 02:08:12 +0800

    调整[下载Mod]：记录所有的Mod名称数据，而不是只记录拥有中文翻译的Mod

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/TranslateInfoItem.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/mod/translate/TranslateInfoItem.kt
@@ -2,6 +2,6 @@ package com.movtery.pojavzh.feature.mod.translate
 
 class TranslateInfoItem(
     val modIndex: String,
-    val chineseName: String,
+    val chineseName: String?,
     val originName: String
 )
