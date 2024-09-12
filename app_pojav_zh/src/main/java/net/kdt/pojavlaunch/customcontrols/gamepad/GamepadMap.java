a3919ee22ede4397ba2cc27897ce193950d23dfc
movte <212824502@qq.com>
2024-09-12 15:09:12 +0800

    调整[设置]：手柄映射绑定使用全键盘弹窗

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/customcontrols/gamepad/GamepadMap.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/customcontrols/gamepad/GamepadMap.java
@@ -191,7 +191,7 @@ public class GamepadMap {
         return new String[]{
                 context.getString(R.string.zh_keycode_unspecified),
                 context.getString(R.string.zh_keycode_mouse_right),
-                context.getString(R.string.zh_keycode_mouse_right),
+                context.getString(R.string.zh_keycode_mouse_middle),
                 context.getString(R.string.zh_keycode_mouse_left),
                 context.getString(R.string.zh_keycode_scroll_up),
                 context.getString(R.string.zh_keycode_scroll_down)};
