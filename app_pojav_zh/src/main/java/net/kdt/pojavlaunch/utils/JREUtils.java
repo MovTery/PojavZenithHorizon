eb919924087844c6825127629e976f7e54f984b3
ShirosakiMio <852468399@qq.com>
2024-09-27 09:40:34 +0800

    修复[Sodium]: 禁用Lwjgl版本检查

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -380,7 +380,8 @@ public class JREUtils {
 
                 "-Dnet.minecraft.clientmodname=" + Tools.APP_NAME,
                 "-Dfml.earlyprogresswindow=false", //Forge 1.14+ workaround
-                "-Dloader.disable_forked_guis=true"
+                "-Dloader.disable_forked_guis=true",
+                "-Dsodium.checks.issue2561=false"
         ));
         if (LauncherPreferences.PREF_ARC_CAPES)
             overridableArguments.add("-javaagent:"+new File(PathAndUrlManager.DIR_DATA,"arc_dns_injector/arc_dns_injector.jar").getAbsolutePath()+"=23.95.137.176");
