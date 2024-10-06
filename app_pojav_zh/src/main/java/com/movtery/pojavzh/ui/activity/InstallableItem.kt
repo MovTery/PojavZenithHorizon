e11e35bfdd7b11283c0699dcf0ed9ed84b3727b7
movte <212824502@qq.com>
2024-10-06 21:22:46 +0800

    调整[启动]：对需要安装的内容进行排序

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/ui/activity/InstallableItem.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/ui/activity/InstallableItem.kt
@@ -8,4 +8,18 @@ class InstallableItem(
     val task: AbstractUnpackTask,
     var isRunning: Boolean = false,
     var isFinished: Boolean = false
-)
+) : Comparable<InstallableItem> {
+
+    override fun compareTo(other: InstallableItem): Int {
+        val thisHasSummary = summary != null
+        val otherHasSummary = other.summary != null
+
+        return if (thisHasSummary && !otherHasSummary) {
+            -1
+        } else if (!thisHasSummary && otherHasSummary) {
+            1
+        } else {
+            name.compareTo(other.name)
+        }
+    }
+}
