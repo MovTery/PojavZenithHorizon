f976e9c153f1fa35bd1c12a060cdb4f7144e44d4
movte <212824502@qq.com>
2024-10-07 18:17:09 +0800

    调整[设置]：检测AdrenoGPU

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/ZHTools.java
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/utils/ZHTools.java
@@ -5,6 +5,11 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
+import android.opengl.EGL14;
+import android.opengl.EGLConfig;
+import android.opengl.EGLContext;
+import android.opengl.EGLDisplay;
+import android.opengl.GLES20;
 import android.os.Bundle;
 import android.os.LocaleList;
 import android.view.View;
@@ -20,6 +25,7 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.movtery.pojavzh.feature.customprofilepath.ProfilePathManager;
+import com.movtery.pojavzh.feature.log.Logging;
 import com.movtery.pojavzh.setting.AllSettings;
 import com.movtery.pojavzh.ui.fragment.FragmentWithAnim;
 
@@ -177,6 +183,65 @@ public final class ZHTools {
     public static String getSystemLanguage() {
         Locale locale = Locale.getDefault();
         return locale.getLanguage() + "_" + locale.getCountry().toLowerCase();
+    }
+
+    public static boolean isAdrenoGPU() {
+        EGLDisplay eglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+        if (eglDisplay == EGL14.EGL_NO_DISPLAY) {
+            Logging.e("CheckVendor", "Failed to get EGL display");
+            return false;
+        }
+
+        if (!EGL14.eglInitialize(eglDisplay, null, 0, null, 0)) {
+            Logging.e("CheckVendor", "Failed to initialize EGL");
+            return false;
+        }
+
+        int[] eglAttributes = new int[]{
+                EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+                EGL14.EGL_NONE
+        };
+
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] numConfigs = new int[1];
+        if (!EGL14.eglChooseConfig(eglDisplay, eglAttributes, 0, configs, 0, 1, numConfigs, 0) || numConfigs[0] == 0) {
+            EGL14.eglTerminate(eglDisplay);
+            Logging.e("CheckVendor", "Failed to choose an EGL config");
+            return false;
+        }
+
+        int[] contextAttributes = new int[]{
+                EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,  // OpenGL ES 2.0
+                EGL14.EGL_NONE
+        };
+
+        EGLContext context = EGL14.eglCreateContext(eglDisplay, configs[0], EGL14.EGL_NO_CONTEXT, contextAttributes, 0);
+        if (context == EGL14.EGL_NO_CONTEXT) {
+            EGL14.eglTerminate(eglDisplay);
+            Logging.e("CheckVendor", "Failed to create EGL context");
+            return false;
+        }
+
+        if (!EGL14.eglMakeCurrent(eglDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, context)) {
+            EGL14.eglDestroyContext(eglDisplay, context);
+            EGL14.eglTerminate(eglDisplay);
+            Logging.e("CheckVendor", "Failed to make EGL context current");
+            return false;
+        }
+
+        String vendor = GLES20.glGetString(GLES20.GL_VENDOR);
+        String renderer = GLES20.glGetString(GLES20.GL_RENDERER);
+        boolean isAdreno = (vendor != null && renderer != null &&
+                vendor.equalsIgnoreCase("Qualcomm") &&
+                renderer.toLowerCase().contains("adreno"));
+
+        // Cleanup
+        EGL14.eglMakeCurrent(eglDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_CONTEXT);
+        EGL14.eglDestroyContext(eglDisplay, context);
+        EGL14.eglTerminate(eglDisplay);
+
+        Logging.d("CheckVendor", "Running on Adreno GPU: " + isAdreno);
+        return isAdreno;
     }
 
     public static void getWebViewAfterProcessing(WebView view) {
