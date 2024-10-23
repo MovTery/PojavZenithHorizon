9e06f5bdbb9e1d3072960c9226628bcd85b14ee8
movte <212824502@qq.com>
2024-10-23 13:45:17 +0800

    修复[设置]：输入框的崩溃问题

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/fragment/settings/wrapper/EditTextSettingsWrapper.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/fragment/settings/wrapper/EditTextSettingsWrapper.kt
@@ -1,10 +1,14 @@
 package com.movtery.zalithlauncher.ui.fragment.settings.wrapper
 
+import android.content.Context
 import android.text.Editable
 import android.text.InputType
 import android.text.TextWatcher
 import android.view.Gravity
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import com.movtery.zalithlauncher.setting.Settings
 
@@ -21,6 +25,17 @@ class EditTextSettingsWrapper(
             setText(value)
             inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_MULTI_LINE
             gravity = Gravity.TOP or Gravity.START
+            setOnEditorActionListener { _, actionId, event ->
+                //处理“确定”按钮的点击事件
+                actionId == EditorInfo.IME_ACTION_DONE || event.action == KeyEvent.ACTION_DOWN
+            }
+            setOnFocusChangeListener { v, hasFocus ->
+                if (!hasFocus) {
+                    // 处理焦点丢失的情况，如收起软键盘
+                    val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
 
             addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(
